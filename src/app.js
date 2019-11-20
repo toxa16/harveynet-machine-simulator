b8ceb0ab153afe49e81436ce54cb414e9fcbf7cc
toxa16 <toxa16@gmail.com>
2019-11-20 23:43:46 +0200

    init connection form

--- src/app.js
+++ src/app.js
@@ -7,9 +7,34 @@ function App() {
   return (
     <div>
       <Navbar />
-      <div className="container mt-5">
-        <h1>App</h1>
-      </div>
+
+      <section className="container mt-5">
+        <div className="row mt-5">
+          <div className="col-12 col-md-8 col-lg-6">
+            <form action="/">
+              <div className="form-group">
+                <label>Machine ID:</label>
+                <input name="machine_id" className="form-control" />
+              </div>
+
+              <div>
+                <button type="submit" className="btn btn-primary">
+                  Connect
+                </button>
+                <button type="button" className="btn btn-danger ml-3" disabled>
+                  Disconnect
+                </button>
+              </div>
+            </form>
+
+            <div className="mt-4">
+              <span>Status:</span>
+              {' '}
+              <b className="text-dark">Disconnected</b>
+            </div>
+          </div>
+        </div>
+      </section>
     </div>
   );
 }
