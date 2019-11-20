3292d1b6d44c44a491ed4c19d2cf0be370f954d1
toxa16 <toxa16@gmail.com>
2019-11-20 23:51:03 +0200

    init connection form component

--- src/app.js
+++ src/app.js
@@ -2,6 +2,7 @@ import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 import Navbar from './navbar/navbar';
+import ConnectionForm from './connection-form/connection-form';
 
 function App() {
   return (
@@ -11,27 +12,7 @@ function App() {
       <section className="container mt-5">
         <div className="row mt-5">
           <div className="col-12 col-md-8 col-lg-6">
-            <form action="/">
-              <div className="form-group">
-                <label>Machine ID:</label>
-                <input name="machine_id" className="form-control" />
-              </div>
-
-              <div>
-                <button type="submit" className="btn btn-primary">
-                  Connect
-                </button>
-                <button type="button" className="btn btn-danger ml-3" disabled>
-                  Disconnect
-                </button>
-              </div>
-            </form>
-
-            <div className="mt-4">
-              <span>Status:</span>
-              {' '}
-              <b className="text-dark">Disconnected</b>
-            </div>
+            <ConnectionForm />
           </div>
         </div>
       </section>
---
+++ src/connection-form/connection-form.js
@@ -0,0 +1,35 @@
+import React from 'react';
+
+export default function ConnectionForm() {
+  function handleSubmit(e) {
+    e.preventDefault();
+    const machineId = e.target.machine_id.value;
+    console.log({ machineId });
+  }
+
+  return (
+    <div>
+      <form onSubmit={handleSubmit}>
+        <div className="form-group">
+          <label>Machine ID:</label>
+          <input name="machine_id" className="form-control" required />
+        </div>
+
+        <div>
+          <button type="submit" className="btn btn-primary">
+            Connect
+          </button>
+          <button type="button" className="btn btn-danger ml-3" disabled>
+            Disconnect
+          </button>
+        </div>
+      </form>
+
+      <div className="mt-4">
+        <span>Status:</span>
+        {' '}
+        <b className="text-dark">Disconnected</b>
+      </div>
+    </div>
+  );
+}
