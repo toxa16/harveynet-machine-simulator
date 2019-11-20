30f2a6c34e766fac4a1ecf057b0cc5fa1f0dfbe4
toxa16 <toxa16@gmail.com>
2019-11-20 23:28:52 +0200

    init navbar component

--- src/app.js
+++ src/app.js
@@ -1,10 +1,15 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
+import Navbar from './navbar/navbar';
+
 function App() {
   return (
     <div>
-      App
+      <Navbar />
+      <div className="container mt-5">
+        <h1>App</h1>
+      </div>
     </div>
   );
 }
---
+++ src/navbar/navbar.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export default function Navbar() {
+  return (
+    <nav className="navbar navbar-expand-lg navbar-dark bg-primary">
+      <div className="container">
+        <span className="navbar-brand">
+          <b>HarveyNet</b> Machine Simulator (v0.0.1)
+        </span>
+      </div>
+    </nav>
+  );
+}
