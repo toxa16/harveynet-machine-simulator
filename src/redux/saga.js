1a41c202bf0444b9c243b2bf37f35f644e3f8ee4
toxa16 <toxa16@gmail.com>
2020-05-11 20:11:45 +0300

    install pusher

--- src/redux/saga.js
+++ src/redux/saga.js
@@ -1,19 +1,40 @@
-import { call, cancel, delay, fork, put, take } from 'redux-saga/effects';
+import { put, take } from 'redux-saga/effects';
+import Pusher from 'pusher-js';
 
 import ActionType from './action-type';
+
+
+const appKey = process.env.REACT_APP_PUSHER_APP_KEY;
+const cluster = process.env.REACT_APP_PUSHER_CLUSTER || 'eu';
+const authEndpoint = process.env.REACT_APP_PUSHER_AUTH_ENDPOINT;
 
 
 /**
  * Root saga.
  */
 export default function* saga() {
+  if (process.env.NODE_ENV === 'development') {
+    Pusher.logToConsole = true;
+  }
   while (true) {
     const connectAction = yield take(ActionType.CONNECT_REQUEST);
-    yield delay(500);
+    const { machineId } = connectAction.payload;
+
+    // init pusher
+    const pusher = new Pusher(appKey, {
+      cluster,
+      authEndpoint,
+      auth: {
+        params: { machineId },
+      },
+    });
+    const channelName = `presence-${machineId}`;
+    
+    pusher.subscribe(channelName);
     yield put({ type: ActionType.CONNECT_SUCCESS });
 
     yield take(ActionType.DISCONNECT_REQUEST);
-    yield delay(500);
+    pusher.unsubscribe(channelName);
     yield put({ type: ActionType.DISCONNECT_SUCCESS });
   }
 }
